d30c26302e48cb0394cd101f1b43c3a1ae167d56
Masami Yonehara <zeitdiebe@gmail.com>
2018-12-29 23:09:19 +0900

    Add Card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react'
+import styled from 'styled-components'
+
+const StyledDiv = styled.div`
+  padding: 10px;
+  height: 100%;
+  width: 100%;
+  border: 1px solid gray;
+  background-color: white;
+  font-size: 10rem;
+  text-align: center;
+`
+
+interface CardProps {
+  questionBody: string
+  answerBody: string
+}
+
+const Card = (props: CardProps) => {
+  const { questionBody } = props
+
+  return (
+    <StyledDiv>
+      <p>{questionBody}</p>
+    </StyledDiv>
+  )
+}
+
+export default Card
--- stories/index.js
+++ stories/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { Button } from '@storybook/react/demo';
+import Card from '../src/components/Card'
 
-storiesOf('Button', module)
-  .add('with text', () => (
-    <Button>Hello Button</Button>
+storiesOf('Card', module)
+  .add('Question', () => (
+    <Card questionBody="test"></Card>
   ))
-  .add('with some emoji', () => (
-    <Button><span role="img" aria-label="so cool">😀 😎 👍 💯</span></Button>
-  ));
-
