bd12a0bda0da664ef7d90320f535e17ad674a065
Masami Yonehara <zeitdiebe@gmail.com>
2019-01-30 19:48:25 +0900

    Introduce redux-devtools-extension

--- src/configureStore.ts
+++ src/configureStore.ts
@@ -1,5 +1,5 @@
 import { createBrowserHistory } from 'history'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import { createLogger } from 'redux-logger'
 import { routerMiddleware } from 'connected-react-router'
@@ -9,14 +9,20 @@ const loggerMiddleware = createLogger()
 
 export const history = createBrowserHistory()
 
+// https://github.com/zalmoxisus/redux-devtools-extension#usage
+const composeEnhancers =
+  (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
 export default function configureStore(preloadedState?: any) {
   return createStore(
     createRootReducer(history),
     preloadedState,
-    applyMiddleware(
-      routerMiddleware(history),
-      thunkMiddleware,
-      loggerMiddleware
+    composeEnhancers(
+      applyMiddleware(
+        routerMiddleware(history),
+        thunkMiddleware,
+        loggerMiddleware
+      )
     )
   )
 }
